399d099eddfc1b87cfb55ff55de6fc50f5f30396
bsleeter <bsleeter@gmail.com>
2019-09-20 00:50:08 -0700

    updated app stock page

--- app.R
+++ app.R
@@ -7,36 +7,54 @@
 #    http://shiny.rstudio.com/
 #
 
-library(extrafont)
+
 library(shiny)
-library(shinythemes)
+library(shinyjs)
 library(dplyr)
 library(ggplot2)
 library(readr)
-library(hrbrthemes)
 library(zoo)
-library(ggiraph)
+library(DT)
 
-loadfonts()
+
 # Process Carbon Stock Data
-stocksEco = read_csv("data/ecoregion_stocks_by_scenario_timestep_95ci.csv") %>% filter(Ecosystem=="Yes")
-stocksEcoTEC = stocksEco %>% group_by(LUC,GCM,RCP,Timestep, EcoregionID, EcoregionName, Ecosystem) %>% summarise(Mean=sum(Mean), Lower=sum(Lower), Upper=sum(Upper)) %>%
+stocksEco = read_csv("data/ecoregion_stocks_by_scenario_timestep_95ci.csv") %>% 
+    filter(Ecosystem=="Yes") %>% 
+    mutate(Mean=Mean/1000, Lower=Lower/1000, Upper=Upper/1000) %>% 
+    select(-EcoregionID)
+
+stocksEcoTEC = stocksEco %>% 
+    group_by(LUC,GCM,RCP,Timestep, EcoregionName, Ecosystem) %>% 
+    summarise(Mean=sum(Mean), Lower=sum(Lower), Upper=sum(Upper)) %>%
     mutate(StockGroup="TEC")
-stocksState = read_csv("data/state_stocks_by_scenario_timestep_95ci.csv") %>% filter(Ecosystem=="Yes") %>%
-    mutate(EcoregionID=0, EcoregionName="State")
-stocksStateTEC = stocksState %>% group_by(LUC,GCM,RCP,Timestep, EcoregionID, EcoregionName, Ecosystem) %>% summarise(Mean=sum(Mean), Lower=sum(Lower), Upper=sum(Upper)) %>%
+
+stocksState = read_csv("data/state_stocks_by_scenario_timestep_95ci.csv") %>% 
+    filter(Ecosystem=="Yes") %>% 
+    mutate(Mean=Mean/1000, Lower=Lower/1000, Upper=Upper/1000) %>%
+    mutate(EcoregionName="State")
+
+stocksStateTEC = stocksState %>% 
+    group_by(LUC,GCM,RCP,Timestep, EcoregionName, Ecosystem) %>% 
+    summarise(Mean=sum(Mean), Lower=sum(Lower), Upper=sum(Upper)) %>%
     mutate(StockGroup="TEC")
-stocks = bind_rows(stocksEco, stocksState, stocksEcoTEC, stocksStateTEC)
+
+stocks = bind_rows(stocksEco, stocksState, stocksEcoTEC, stocksStateTEC) %>%
+    mutate(Mean=round(Mean,3), Lower=round(Lower,3), Upper=round(Upper,3))
 
 # Process Net Flux List
-fluxEco = read_csv("data/ecoregion_netflux_by_scenario_timestep_95ci.csv")
-netFluxEco = fluxEco %>% filter(Flux %in% c("NPP","Rh","NEP","NECB"))
+fluxEco = read_csv("data/ecoregion_netflux_by_scenario_timestep_95ci.csv") %>% 
+    select(-EcoregionID)
+
+netFluxEco = fluxEco %>% 
+    filter(Flux %in% c("NPP","Rh","NEP","NECB"))
 
 fluxState = read_csv("data/state_netflux_by_scenario_timestep_95ci.csv")
-netFluxState = fluxState %>% filter(Flux %in% c("NPP","Rh","NEP","NECB")) %>% mutate(EcoregionID=0, EcoregionName="State")
-netFlux = bind_rows(netFluxEco, netFluxState)
-unique(netFluxState$Flux)
 
+netFluxState = fluxState %>% 
+    filter(Flux %in% c("NPP","Rh","NEP","NECB")) %>% 
+    mutate(EcoregionName="State")
+
+netFlux = bind_rows(netFluxEco, netFluxState)
 
 ecoList = unique(stocks$EcoregionName)
 lucList = unique(stocks$LUC)
@@ -44,64 +62,63 @@ stockList = unique(stocks$StockGroup)
 
 # Define UI for application that draws a histogram
 ui = fluidPage(
+    useShinyjs(),
        includeHTML("www/header.html"),
-       tags$head(includeCSS("www/common.css")),   
-       titlePanel("California Carbon Scenarios"),
+       tags$head(includeCSS("www/common.css")),
+    
+    titlePanel("California Carbon Scenarios"),
        
        sidebarLayout(
            
-           sidebarPanel(
+           sidebarPanel(style = "background: #00264c; color: #ffffff",
                h3("Carbon Stocks"), 
                width=3,
                selectInput("ecoregion", label=h4("Region"), choices=unique(stocks$EcoregionName), selected="State"),
                selectInput("luc", label=h4("Land Use Scenario"), choices=unique(stocks$LUC), selected="BAU"),
                checkboxGroupInput("rcp", label=h4("Climate Scenario"),choiceValues=unique(stocks$RCP),choiceNames=c("Low Emissions (RCP 4.5)", "High Emissions (RCP 8.5)"), selected="rcp45"),
                checkboxGroupInput("gcm",label=h4("Climate Model"),choiceValues=unique(stocks$GCM),choiceNames=c("Average (CanESM2)","Warm-Wet (CNRM-CM5)", "Hot-Dry (HadGEM2-ES)", "Complement (MIROC5)"), selected="CanESM2"),
-               sliderInput("years", label=h4("Year Range"), min=2001, max=2100, value=c(2001,2100), sep="", width="100%")
-               
-           ),
+               sliderInput("years", label=h4("Year Range"), min=2001, max=2100, value=c(2001,2100), sep="", width="100%"),
+               checkboxInput("ci1", "Toggle 95% Confidence Intervals", value=TRUE)),
            
            mainPanel(
-               tabsetPanel(
-                   tabPanel("Carbon Stocks",value = "Carbon StocksProjected Carbon Storage in California",
-                            width=9,
-                            selectInput("stockGroup",label=h4("Carbon Stock"), choices=unique(stocks$StockGroup),selected="TEC"),
-                            checkboxInput("ci1", "Toggle 95% Confidence Intervals", value=TRUE),
-                            plotOutput("stocksPlot1", height="400"),
-                            plotOutput("stocksPlot2", height="600")
-                                
-                        ),
                
-                   
-                   
-                   
-                   tabPanel("Net Carbon Fluxes", value="Net Carbon Fluxes", icon=icon("calendar"),
-                            width=9,
-                            selectInput("netFlux",label=h4("Net Flux"), choices=unique(netFlux$Flux),selected="NECB"),
-                            checkboxInput("smooth", "Add trend line", value=FALSE),
-                            checkboxInput("ci", "Add 95% Confidence Intervals", value=FALSE),
-                            checkboxInput("annual", "Add Annual Projections", value=FALSE),
-                            plotOutput("fluxplot1", height="400"),
-                            plotOutput("fluxplot2", height="400")
-                           
-                   ),
-                   tabPanel("Landcover Totals", value="Landcover Totals",
+               tabsetPanel(
+                   tabPanel("Carbon Stocks",value = "Carbon StocksProjected Carbon Storage in California", width=12, 
                             
-                            titlePanel("Landcover Totals")
-                   ),
-                   tabPanel("Landcover Transition", value="Landcover Transition",
+                       wellPanel(style = "background: #ffffff",
+                           fluidRow(     
+                            column(width=6, tags$br(),
+                                   selectInput("stockGroup",label="Select Carbon Stock Type", choices=unique(stocks$StockGroup),selected="TEC")))),
+                       
+                       wellPanel(style = "background: #f3f3f3",
+                           fluidRow(
+                            column(width=12, 
+                                   plotOutput("stocksPlot1", height="400", hover = hoverOpts("stocksPlot1_hover", delay = 20, delayType = "debounce")),
+                                   uiOutput("stocksPlot1_hover_info"),
+                                   checkboxInput("showStockTable","View/Download Chart Data", FALSE),
+                                   DTOutput("stocktable")))),
+                       
+                       wellPanel(style = "background: #f3f3f3",
+                           fluidRow(       
+                            column(width=12,
+                                   plotOutput("stocksPlot2", height="600", hover = hoverOpts("stocksPlot2_hover", delay = 20, delayType = "debounce")),
+                                   uiOutput("stocksPlot2_hover_info"))))),
+                   
+                   tabPanel("Net Carbon Fluxes", value="Net Carbon Fluxes", icon=icon("calendar"), width=12,
                             
-                            titlePanel("Landcover Transition")
-                   )
+                            column(width=12, 
+                                   selectInput("netFlux",label=h4("Net Flux"), choices=unique(netFlux$Flux),selected="NECB"),
+                                   checkboxInput("ci", "Add 95% Confidence Intervals", value=FALSE),
+                                   checkboxInput("annual", "Add Annual Projections", value=FALSE)),
+                            column(width=12,
+                                   plotOutput("fluxplot1", height="400"),
+                                   plotOutput("fluxplot2", height="400"))),
                    
-                   #
+                   tabPanel("Landcover Totals", value="Landcover Totals"),
                    
-                                          
-               )
-              
-           )
-           
-       ),
+                   tabPanel("Landcover Transition", value="Landcover Transition")))),
+       
+       
        includeHTML("www/footer.html")
        
 )
@@ -119,28 +136,72 @@ server = (function(input, output) {
                           filter(Timestep>=input$years[1], Timestep<=input$years[2])
     })
     
+    output$stocktable = renderDT(server = FALSE, {
+        DT::datatable(selectData1(),  
+                                 extensions='Buttons', 
+                                 options=list(pageLength=5, searching=TRUE, autoWidth=TRUE, ordering=TRUE, dom ='Blfrtip',buttons=c('copy', 'csv', 'excel', "pdf")))
+    })
+    
+    observeEvent(input$showStockTable, {
+        toggle("stocktable")
+    })
+
+    
     output$stocksPlot1 <-  renderPlot({
-        p1 = ggplot(data=selectData1(), aes(x=Timestep, y=Mean/1000, fill=GCM, color=GCM)) +
+        p1 = ggplot(data=selectData1(), aes(x=Timestep, y=Mean, fill=GCM, color=GCM)) +
             geom_line() +
-            geom_point() +
-            scale_fill_ipsum() +
-            scale_color_ipsum() +
-            facet_wrap(RCP~LUC) +
-            theme_ipsum_rc(14, grid="Y") +
-            labs(x="Year", y="Million Metric Tons of Carbon", title="Total Ecosystem Carbon Storage by Scenario", subtitle="Total carbon stored in Live, Dead, and Soil pools") +
-            theme(legend.position = "top", plot.margin=margin(5,5,5,5)) 
+            geom_point(size=2, shape=16, data=filter(selectData1(), Timestep %in% seq(2001,2101,1))) +
+            scale_fill_brewer(palette = "Dark2") +
+            scale_color_brewer(palette = "Dark2") +
+            facet_wrap(~RCP) +
+            theme_light(18) +
+            labs(y="MMT C", x="", title="Total Ecosystem Carbon Storage by Scenario") +
+            theme(legend.position = "bottom", 
+                  plot.margin=margin(5,5,5,5),
+                  legend.title = element_blank(), 
+                  strip.background = element_rect(fill="#00264c", color="#00264c"),
+                  panel.grid.major = element_line(linetype = "solid", size=0.5),
+                  panel.grid.minor = element_line(linetype = "dashed", size=0.1),
+                  panel.border = element_rect(size = 1, colour = "#00264c"),
+                  plot.title = element_text(hjust=0.5, face="bold"),
+                  axis.title = element_text(size = 14),
+                  plot.background = element_rect(fill="#f3f3f3", color="#f3f3f3"),
+                  legend.background = element_rect(fill="#f3f3f3", color="#f3f3f3"))
         
         if(input$ci1)
-            p1 = p1 + geom_ribbon(aes(ymin=Lower/1000, ymax=Upper/1000), alpha=0.5, color=NA)
+            p1 = p1 + geom_ribbon(aes(ymin=Lower, ymax=Upper), alpha=0.5, color=NA)
         
         p1 
         
     })
     
-   
+    output$stocksPlot1_hover_info <- renderUI({
+        hover <- input$stocksPlot1_hover
+        
+        point <- nearPoints(selectData1(), hover, threshold = 20, maxpoints = 1, addDist = TRUE)
+        if (nrow(point) == 0) return(NULL)
+        left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
+        top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
+        left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
+        top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
+        style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.95); ",
+                        "left:", left_px + 2, "px; top:", top_px + 2, "px;")
+        wellPanel(
+            style = style,
+            p(HTML(paste0(
+                "<b> Timestep: </b>", point$Timestep, "<br/>",
+                "<b> Landuse Scenario: </b>", point$LUC, "<br/>",
+                "<b> RCP: </b>", point$RCP, "<br/>",
+                "<b> Climate Model: </b>", point$GCM, "<br/>",
+                "<b> Stock Group: </b>", point$StockGroup, "<br/>",
+                "<b> Mean Carbon (MMT): </b>", round(point$Mean,1), "<br/>",
+                "<b> Lower Bound (MMT): </b>", round(point$Lower,1), "<br/>",
+                "<b> Upper Bound (MMT): </b>", round(point$Upper,1), "<br/>"))))
+    })
     
     
-    
+
+##### Stock Change Plot #####    
     
     selectData2 = reactive({
         stocks %>% filter(Ecosystem=="Yes", LUC %in% input$luc, GCM %in% input$gcm,  RCP %in% input$rcp, EcoregionName==input$ecoregion) %>%
@@ -148,18 +209,61 @@ server = (function(input, output) {
     })
     
     output$stocksPlot2 <- renderPlot({
-        p2 = ggplot(data=selectData2(), aes(x=StockGroup, y=MeanChange/1000, fill=StockGroup, color=StockGroup)) +
-            geom_bar(stat="identity") + 
-            scale_fill_ipsum() +
-            scale_color_ipsum() +
-            facet_grid(GCM~RCP) +
-            theme_ipsum_rc(14, grid="Y") +
-            labs(x="Year", y="Million Metric Tons of Carbon", title="Average Projected Ecosystem Carbon Storage by Carbon Pool", subtitle="Living biomass (Live), dead organic matter (DOM), and soil organic carbon (SOC) storage") +
-            theme(legend.position = "top", plot.margin=margin(30,5,5,5))
+        p2 = ggplot(data=selectData2(), aes(x=RCP, y=MeanChange, fill=StockGroup, color=StockGroup)) +
+            geom_bar(stat="identity", color="black", position="dodge") + 
+            scale_fill_brewer(palette = "Accent") +
+            scale_color_brewer(palette = "Accent") +
+            facet_wrap(~GCM, ncol=4) +
+            theme_light(18) +
+            labs(x="RCP Scenario", y="MMT C", title="Net Change in Carbon Stocks by Scenario") +
+            theme(legend.position = "bottom", 
+                  plot.margin=margin(5,5,5,5),
+                  legend.title = element_blank(), 
+                  strip.background = element_rect(fill="#00264c", color="#00264c"),
+                  panel.grid.major = element_line(linetype = "solid", size=0.5),
+                  panel.grid.minor = element_line(linetype = "dashed", size=0.1),
+                  panel.border = element_rect(size = 1, colour = "#00264c"),
+                  plot.title = element_text(hjust=0.5, face="bold"),
+                  axis.title = element_text(size = 14),
+                  plot.background = element_rect(fill="#f3f3f3", color="#f3f3f3"),
+                  legend.background = element_rect(fill="#f3f3f3", color="#f3f3f3"))
+        
         if(input$ci1)
-            p2 = p2 + geom_errorbar(aes(ymin=LowerChange/1000, ymax=UpperChange/1000), color="black", width=0.5) 
+            p2 = p2 + geom_errorbar(aes(ymin=LowerChange, ymax=UpperChange), color="black", width=0.9, position = "dodge") 
         p2
         
+    })
+    
+    
+    output$stocksPlot2_hover_info <- renderUI({
+        hover <- input$stocksPlot2_hover
+        
+        point <- nearPoints(selectData2(), hover, threshold = 20, maxpoints = 1, addDist = TRUE)
+        if (nrow(point) == 0) return(NULL)
+        left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
+        top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
+        left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
+        top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
+        
+        
+        style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.95); ",
+                        "left:", left_px + 2, "px; top:", top_px + 2, "px;")
+        
+        wellPanel(
+            style = style,
+            p(HTML(paste0(
+                "<b> Timestep: </b>", point$Timestep, "<br/>",
+                "<b> Landuse Scenario: </b>", point$LUC, "<br/>",
+                "<b> RCP: </b>", point$RCP, "<br/>",
+                "<b> Climate Model: </b>", point$GCM, "<br/>",
+                "<b> Stock Group: </b>", point$StockGroup, "<br/>",
+                "<b> Mean Carbon (MMT): </b>", round(point$Mean,1), "<br/>",
+                "<b> Lower Bound (MMT): </b>", round(point$Lower,1), "<br/>",
+                "<b> Upper Bound (MMT): </b>", round(point$Upper,1), "<br/>"
+                
+                
+            )))
+        )
     })
     
     
@@ -176,10 +280,10 @@ server = (function(input, output) {
         p = ggplot(data=selectData3(), aes(x=Timestep, y=Mean10, fill=GCM, color=GCM)) +
             geom_line(size=1) + 
             geom_hline(yintercept=0, color="black", size=0.5) +
-            scale_fill_ipsum() +
-            scale_color_ipsum() +
+            scale_fill_brewer() +
+            scale_color_brewer() +
             facet_wrap(RCP~LUC) +
-            theme_ipsum_rc(20, grid="Y") +
+            theme_minimal(20) +
             labs(x="Year", y="Million Metric Tons of Carbon", title="Net Ecosystem Carbon Flux by Scenario", subtitle="Rolling annual 10-year average net carbon fluxes by scenario") +
             theme(legend.position = "top", plot.margin=margin(5,5,5,5))
         
@@ -205,10 +309,10 @@ server = (function(input, output) {
         p4 = ggplot(data=selectData4(), aes(x=GCM, y=Mean, fill=GCM, color=GCM)) +
             geom_bar(stat="identity") + 
             geom_hline(yintercept=0) +
-            scale_fill_ipsum() +
-            scale_color_ipsum() +
+            scale_fill_brewer() +
+            scale_color_brewer() +
             facet_wrap(RCP~LUC) +
-            theme_ipsum_rc(20, grid="Y") +
+            theme_minimal(20) +
             labs(y="Million Metric Tons of Carbon", title="Net Ecosystem Carbon Balance (NECB) by Scenario", subtitle="Rolling annual 10-year average net carbon fluxes by scenario") +
             theme(legend.position = "top", plot.margin=margin(5,5,5,5))
         
